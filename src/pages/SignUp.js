34b49362477eb0e95e8e61e772988551fa69edb2
Mohammed Heggy <hegy395@gmail.com>
2023-03-27 03:08:49 +0200

    repair all files which was destroyed

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,22 +1,22 @@
 import Button from "../components/UI/Button";
 import FormInput from "../components/UI/FormInput";
-import classes from "./Login.module.css";
+import classes from "./SignUp.module.css";
 import { Link } from "react-router-dom";
 
 const SignUp = (props) => {
   return (
     <main>
-      <form>
-        <FormInput placeholder="Full Name" />
-        <FormInput type="email" placeholder="E-Mail" />
-        <FormInput type="password" placeholder="Password" />
-        <Button type="submit">Sign Up</Button>
+      <form className={classes.form}>
+        <FormInput placeholder="Full Name" className={classes.name}/>
+        <FormInput type="email" placeholder="E-Mail" className={classes.email}/>
+        <FormInput type="password" placeholder="Password" className={classes.password}/>
+        <Button type="submit" className={classes.signup}>Sign Up</Button>
+        <div className={classes.note}>
+          <p>
+            Already have an account?<Link to="/login"><span>Login</span></Link>
+          </p>
+        </div>
       </form>
-      <div className={classes.note}>
-        <p>
-          Already have an account?<Link to="/login">Login</Link>
-        </p>
-      </div>
     </main>
   );
 };
