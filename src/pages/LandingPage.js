10a318afad27f885e33cf959261823de275b4b2c
Mohammed Heggy <hegy395@gmail.com>
2023-04-13 03:14:01 +0200

    blue pallete

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,12 @@
 import CourseList from "../components/courses/CourseList";
 import classes from "./LandingPage.module.css";
-import landingImage from "../assets/landing-page.jfif";
+// import landingImage from "../assets/landing-page.jfif";
 
 const LandingPage = (props) => {
   return (
     <main className={classes["landing-page"]}>
       <div className={classes["landing-image"]}>
-        <img src={landingImage} alt="Nice Desktop" />
+        {/* <img src={landingImage} alt="Nice Desktop" /> */}
       </div>
       <div className={classes["landing-courses"]}>
         <CourseList 
