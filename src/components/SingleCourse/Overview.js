438c6fd166823aa91daa2167b78be7fa95f1c7c8
Amr <amr.nasem51@gmail.com>
2023-08-04 19:12:25 +0300

    Fix the layout of single course page

--- src/components/SingleCourse/Overview.js
+++ src/components/SingleCourse/Overview.js
@@ -7,7 +7,6 @@ import CupIcon from "../Icons/CupIcon";
 import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../store/cart-slice";
 import React, { useEffect, useState } from "react";
-import Preview from "./Preview";
 
 const Overview = (props) => {
   let numOfArticles = 0;
@@ -46,79 +45,77 @@ const Overview = (props) => {
   };
 
   return (
-    <div className={classes.track}>
-      <div className={`${classes.overview} ${props.hide && classes.float}`}>
-        <Preview className={props.hide ? classes.hide : classes.display} />
-        <div className={classes.body}>
-          <div className={classes.price}>
-            <span className={classes["final-price"]}>
-              ${course.price - course.price * (course.discount / 100)}
-            </span>
-            <span className={classes["initial-price"]}>
-              <del>${course.price}</del>
-            </span>
-            <span className={classes.discount}>{course.discount}% off</span>
-          </div>
-          <button onClick={addToCartHandler} className={classes["add-to-cart"]}>
-            {cartItems[course.id] ? "Remove from cart" : "Add to cart"}
-          </button>
-          <button className={classes["buy"]}>Buy now</button>
-          <h6 className={classes.refund}>30-Day Money-Back Guarantee</h6>
-          <div className={classes["course-features"]}>
-            <h5>This course includes:</h5>
-            <ul>
+    <div
+      className={`d-none d-lg-block ${classes.overview} ${props.className} ${
+        props.hide && classes.float
+      }`}
+    >
+      {props.Preview}
+      <div className={classes.body}>
+        <div className={classes.price}>
+          <span className={classes["final-price"]}>
+            ${course.price - course.price * (course.discount / 100)}
+          </span>
+          <span className={classes["initial-price"]}>
+            <del>${course.price}</del>
+          </span>
+          <span className={classes.discount}>{course.discount}% off</span>
+        </div>
+        <button onClick={addToCartHandler} className={classes["add-to-cart"]}>
+          {cartItems[course.id] ? "Remove from cart" : "Add to cart"}
+        </button>
+        <button className={classes["buy"]}>Buy now</button>
+        <h6 className={classes.refund}>30-Day Money-Back Guarantee</h6>
+        <div className={classes["course-features"]}>
+          <h5>This course includes:</h5>
+          <ul>
+            <li>
+              <VideoIcon />
+              <span className={classes.ListItem}>65 hours on-demand video</span>
+            </li>
+            {numOfArticles > 0 && (
               <li>
-                <VideoIcon />
-                <span className={classes.ListItem}>
-                  65 hours on-demand video
-                </span>
+                <FileIcon />
+                <span>{numOfArticles} articles</span>
               </li>
-              {numOfArticles > 0 && (
-                <li>
-                  <FileIcon />
-                  <span>{numOfArticles} articles</span>
-                </li>
-              )}
-              <li>
-                <InfinityIcon />
-                <span className={classes.ListItem}>Full lifetime access</span>
-              </li>
-              <li>
-                <TVIcon />
-                <span className={classes.ListItem}>
-                  Access on mobile and TV
-                </span>
-              </li>
-              <li>
-                <CupIcon />
-                <span className={classes.ListItem}>
-                  Certifictate of completion
-                </span>
-              </li>
-            </ul>
-            <div>
-              {isCopied && <div className={classes.popup}>Link Copied!</div>}
-              <button onClick={copyURL} className={classes.share}>
-                Share
-              </button>
-              <button
-                onClick={() => setApplyCoupon((prevState) => !prevState)}
-                className={classes.coupon}
-              >
-                Apply Coupon
-              </button>
-            </div>
-            {applyCoupon && (
-              <form className={classes.couponform}>
-                <input
-                  placeholder="Enter coupon"
-                  type="text"
-                  className={classes.ApplyCoupon}
-                />
-                <button className={classes.ApplyCouponButton}>Apply</button>
-              </form>
             )}
+            <li>
+              <InfinityIcon />
+              <span className={classes.ListItem}>Full lifetime access</span>
+            </li>
+            <li>
+              <TVIcon />
+              <span className={classes.ListItem}>Access on mobile and TV</span>
+            </li>
+            <li>
+              <CupIcon />
+              <span className={classes.ListItem}>
+                Certifictate of completion
+              </span>
+            </li>
+          </ul>
+          <div>
+            {isCopied && <div className={classes.popup}>Link Copied!</div>}
+            <button onClick={copyURL} className={classes.share}>
+              Share
+            </button>
+            <button
+              onClick={() => setApplyCoupon((prevState) => !prevState)}
+              className={classes.coupon}
+            >
+              Apply Coupon
+            </button>
           </div>
+          {applyCoupon && (
+            <form className={classes.couponform}>
+              <input
+                placeholder="Enter coupon"
+                type="text"
+                className={classes.ApplyCoupon}
+              />
+              <button className={classes.ApplyCouponButton}>Apply</button>
+            </form>
+          )}
         </div>
       </div>
     </div>
